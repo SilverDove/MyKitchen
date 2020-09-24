f587157af8d950f9344d8d73e15cd33bf0916fa7
SilverDove <tricot.clara99@gmail.com>
2020-09-24 16:32:12 +0200

    Add Adapter for the RecyclerView

--- app/src/main/java/com/example/mykitchen/MainActivity.kt
+++ app/src/main/java/com/example/mykitchen/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.mykitchen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val exampleList = generateDummyList(500)
+
+        recycler_view.adapter = MyKitchenAdapter(exampleList)
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.setHasFixedSize(true)//Optimize performance when list size is fixed
+
+    }
+
+    private fun generateDummyList(size: Int): List<ItemsClass>{
+        val list = ArrayList<ItemsClass>()
+
+        for (i in 0 until size){
+            val drawable = when (i%3){
+                0 -> R.drawable.ic_launcher_background
+                1 -> R.drawable.ic_launcher_foreground
+                else -> R.drawable.ic_launcher_background
+            }
+            val item = ItemsClass(drawable, "Titel $i", "Description")
+            list += item
+        }
+
+        return list
     }
 }
---
+++ app/src/main/java/com/example/mykitchen/MyKitchenAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.mykitchen
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import kotlinx.android.synthetic.main.items.view.*
+
+class MyKitchenAdapter(private val list: List<ItemsClass>) : Adapter<MyKitchenAdapter.MyKitchenViewHolder>() {
+
+    //Call by the RecyclerView when it is time to create a new ViewHolder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyKitchenViewHolder { //ViewType when we want to use several type of row layout in the recyclerview
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.items,
+            parent,  false)
+
+        //return one of our MyKitchenViewHolder
+        return MyKitchenViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyKitchenViewHolder, position: Int) {//What data do we put in our recyclerView?
+        val currentItem = list[position]
+
+        //Fill data depending on the index
+        holder.imageView.setImageResource(currentItem.imageRessource);
+        holder.title.text = currentItem.title
+        holder.desc.text = currentItem.des
+    }
+
+    //return the number of items in the list
+    override fun getItemCount() = list.size
+
+    class MyKitchenViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        //Create properties storing the references to the Views on our row layout
+        val imageView: ImageView = itemView.image_view //equivalent of findViewById(R.id.image_view)
+        val title: TextView = itemView.title_view
+        val desc: TextView = itemView.description_view
+    }
+}
