1d169b9fe5887ad1e2fa97663e97a645142bf38a
SilverDove <tricot.clara99@gmail.com>
2020-11-21 14:47:28 +0100

    Add Serach Bar

--- app/src/main/java/com/example/mykitchen/RecipeApiService.kt
+++ app/src/main/java/com/example/mykitchen/RecipeApiService.kt
@@ -7,9 +7,9 @@ import retrofit2.http.Query
 
 interface RecipeApiService {
     @GET("complexSearch")
-    fun getSearchResult(@Query("apiKey") apiKey: String, @Query("query") query: String) : Call<RecipeResponse>
+    fun getSearchResult(@Query("apiKey") apiKey: String, @Query("query") query: String?) : Call<RecipeResponse>
 
     @GET("{id}/information")
-    fun getRecipeInformation(@Path("id") id: Int, @Query("apiKey") apiKey: String) : Call<RecipeDetails>
+    fun getRecipeInformation(@Path("id") id: Int, @Query("apiKey") apiKey: String?) : Call<RecipeDetails>
 
 }
