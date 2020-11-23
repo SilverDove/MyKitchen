4f1346846b34d1b31f66ac8aed53e547837f43c7
SilverDove <tricot.clara99@gmail.com>
2020-11-23 18:57:58 +0100

    Fix image display issue using Picasso

--- app/src/main/java/com/example/mykitchen/MainActivity.kt
+++ app/src/main/java/com/example/mykitchen/MainActivity.kt
@@ -4,13 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.items.view.*
 import org.koin.android.ext.android.inject
 import retrofit2.Call
 import retrofit2.Callback
@@ -102,26 +100,12 @@ class MainActivity : AppCompatActivity(), MyKitchenAdapter.OnItemClickListener {
     }
 
     private fun displayList(){
-        val exampleList: List<ItemsClass> = generateItemList()
-        recycler_view.adapter = MyKitchenAdapter(exampleList, this)
+        recycler_view.adapter = MyKitchenAdapter(ListRecipe, this, this)
         recycler_view.layoutManager = LinearLayoutManager(this)
         recycler_view.setHasFixedSize(true)//Optimize performance when list size is fixed
     }
 
-    private fun generateItemList(): List<ItemsClass>{
-        val list = ArrayList<ItemsClass>()
-        //TODO: display picture
-
-        for (i in 0 until ListRecipe.size){
-            val item = ItemsClass(DownloadImageFromPath(ListRecipe.get(i).image),ListRecipe.get(i).title, "bla bla")
-            list += item
-        }
-
-        return list
-    }
-
     override fun onItemClick(position: Int) {//Go to another activity after clicking on the item
-        //TODO: Go to another activity
         Toast.makeText(this, "You click the item number $position", Toast.LENGTH_SHORT).show()
         val intent = Intent(this, details::class.java)
         intent.putExtra(ID_NUMBER_INTENT, ListRecipe.get(position).id)
